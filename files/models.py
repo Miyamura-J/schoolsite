ad1efad497761d579b926fbb9e4a1ae365ed69b7
JoyBoy <miyamura.maj@gmail.com>
2022-10-10 23:05:57 +0000

    Grade, major, module, and document tables
    Added the tables mentioned in the title,
    but still need to make a many-to-many relationship between majors and modules

--- files/models.py
+++ files/models.py
@@ -16,3 +16,28 @@ class Note(db.Model):
     date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+class Grade(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    majors = db.relationship("Major")
+    modules = db.relationship("Module")
+
+class Major(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    grade_id = db.Column(db.Integer, db.ForeignKey('grade.id'))
+    modules = db.relationship("Module")
+
+# I could have one module for many majors and many modules for one majors. Many-to-many relationship to make
+class Module(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    grade_id = db.Column(db.Integer, db.ForeignKey('grade.id'), nullable=True)
+    major_id = db.Column(db.Integer, db.Foreignkey('major.id'), nullable=True)
+    documents = db.relationship("Document")
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+    document = db.Column(db.String(50), unique=True)
+    module_id = db.Column(db.Integer, db.Foreignkey('module.id'))
