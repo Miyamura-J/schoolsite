65f26c0c91ac467fe84164e81d6a9fc61d4749af
JoyBoy <miyamura.maj@gmail.com>
2022-10-24 22:48:32 +0000

    Modules files and fill tables at creation
    Major, Year, and Module tables are filled at creation of db

--- files/__init__.py
+++ files/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path, listdir
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -36,3 +36,34 @@ def create_db(app):
         with app.app_context():
             db.create_all()
         print("Database Created!")
+
+        from .models import Major, Year, Module, Document
+        major_names = ["CYCLE PRÉPARATOIRE","G INFORMATIQUE","G INDUSTRIEL","G DES PROCÉDÉS ENERGIE ET ENVIRONNEMENT","G ELECTRIQUE","G CIVIL","G MÉCANIQUE"]
+        with app.app_context():
+            for major_name in major_names:
+                if not Major.query.filter_by(name=major_name).first():
+                    new_major = Major(name=major_name)
+                    db.session.add(new_major)
+
+            majors_abb = [name.split("_")[1] for name in sorted(listdir("modules/"))]
+            for i in range(len(majors_abb)):
+                for j in range(1,4):
+                    year_name = majors_abb[i]+" "+str(j)
+                    if not Year.query.filter_by(name=year_name).first() and year_name!="CP 3":
+                        new_year = Year(name=year_name,major_id=i+1)
+                        db.session.add(new_year)
+                
+            module_files = sorted(listdir("modules/"))
+            for file in module_files:
+                file = open("modules/"+file,'r')
+                for line in file.readlines():
+                    line = line.replace("\n", "")
+                    if line[0:2] == "##":
+                        pass
+                    elif line[0] == "#":
+                        pass
+                    elif not Module.query.filter_by(name=line).first():
+                        module_name = line
+                        db.session.add(Module(name=module_name))
+
+            db.session.commit()
